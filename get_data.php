1c72afb90d7add6c03d65567e181ad24f0cf683d
novilianaramadani <avi.djabar@gmail.com>
2024-05-06 10:01:58 +0700

    3. Menampilkan data dalam bentuk tabel

--- get_data.php
+++ get_data.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'dinas_adkek_palu';
+$database = 'dinas_admkek_palu';
 $koneksi = new mysqli($host, $username, $password, $database);
 
 // koneksi
